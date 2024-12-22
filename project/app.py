30e3e86914aa5b8776e4ff29411a5bfa844f4297
Amr Abed <amraabed@gmail.com>
2024-12-21 20:07:12 -0500

    Use click

--- project/app.py
+++ project/app.py
@@ -1,5 +1,10 @@
-def main():
-    print("Hello World!")
+from click import command, option
+
+
+@command(context_settings={"help_option_names": ["-h", "--help"]}, help="Say hello")
+@option("-n", "--name", default="World", help="Name", show_default=True)
+def main(name: str = "World"):
+    print(f"Hello {name}!")
 
 
 if __name__ == "__main__":
